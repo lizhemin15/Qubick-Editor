0e36d529e8068e46097e7eb73bfe5440565e7b81
jamily <lizhemin15@126.com>
2025-01-14 17:49:10 +0800

    删除react
    让ai写react，错误漏洞百出，还是回归纯html,css,js吧，把分离做好

--- qubick-editor-react/install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-npm install
-npm install --save-dev @types/react @types/react-dom @types/node
-npm install @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome
-npm install react-beautiful-dnd @types/react-beautiful-dnd 
